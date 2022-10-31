2f2f95b11a0e153051e571b7548e615071fc15a5
Tea Koscina <teax0669@edu.zealand.dk>
2022-10-31 10:56:16 +0100

    Add project files.

---
+++ RestFB/Controllers/FootballPlayersController.cs
@@ -0,0 +1,51 @@
+﻿using Assingment1_FootballPlayers;
+using Microsoft.AspNetCore.Mvc;
+using RestFB.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestFB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayersController : ControllerBase
+    {
+        private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+        // GET: api/<FootballPlayerController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<List<FootballPlayer>> Get()
+        {
+            List<FootballPlayer> players = _manager.GetAll();
+
+            if (players.Count() == 0) return NoContent();
+            else { return Ok(players); }
+        }
+
+        // GET api/<FootballPlayerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FootballPlayerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FootballPlayerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FootballPlayerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RestFB/Managers/FootballPlayersManager.cs
@@ -0,0 +1,52 @@
+﻿using Assingment1_FootballPlayers;
+
+namespace RestFB.Managers
+{
+    public class FootballPlayersManager
+    {
+        private static int _nextId = 1;
+        public static List<FootballPlayer> players = new List<FootballPlayer>()
+        {
+            new FootballPlayer{ Id=_nextId++, Name="Ante", Age=28, ShirtNumber=13},
+            new FootballPlayer{ Id=_nextId++, Name="Mate", Age=24, ShirtNumber=32},
+            new FootballPlayer{ Id=_nextId++, Name="Jaksa", Age=22, ShirtNumber=13},
+            new FootballPlayer{ Id=_nextId++, Name="Luka", Age=21, ShirtNumber=45}
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            List<FootballPlayer> playerList = new List<FootballPlayer>(players);
+            return playerList;
+        }
+
+        public FootballPlayer GetByID(int id)
+        {
+            return players.Find(p => p.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newPlayer)
+        {
+            newPlayer.Id = _nextId++;
+            players.Add(newPlayer);
+            return newPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer p = GetByID(id);
+            if (id == null) return null;
+            players.Remove(p);
+            return p;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer p = GetByID(id);
+            if (p == null) return null;
+            p.Id = updates.Id;
+            p.Name = updates.Name;
+            p.ShirtNumber = updates.ShirtNumber;
+            return p;
+        }
+    }
+}
